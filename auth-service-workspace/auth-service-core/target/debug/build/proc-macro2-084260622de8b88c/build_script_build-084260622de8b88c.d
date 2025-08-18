91e68128b12f4a3bd7793b130a9cc8d8e8180296
Albar <GalacticHyperStar@gmail.com>
2025-08-18 14:37:07 -0400

    fix: remove build target folder

--- auth-service-workspace/auth-service-core/target/debug/build/proc-macro2-084260622de8b88c/build_script_build-084260622de8b88c.d
+++
@@ -1,5 +0,0 @@
-D:\Projects\Rust\live-bootcamp-project\auth-service-core\target\debug\build\proc-macro2-084260622de8b88c\build_script_build-084260622de8b88c.d: D:\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.101\build.rs
-
-D:\Projects\Rust\live-bootcamp-project\auth-service-core\target\debug\build\proc-macro2-084260622de8b88c\build_script_build-084260622de8b88c.exe: D:\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.101\build.rs
-
-D:\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.101\build.rs:
--- auth-service-workspace/auth-service-core/target/debug/build/serde-b4663492006c9465/build_script_build-b4663492006c9465.d
+++
@@ -1,5 +0,0 @@
-D:\Projects\Rust\live-bootcamp-project\auth-service-core\target\debug\build\serde-b4663492006c9465\build_script_build-b4663492006c9465.d: D:\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.219\build.rs
-
-D:\Projects\Rust\live-bootcamp-project\auth-service-core\target\debug\build\serde-b4663492006c9465\build_script_build-b4663492006c9465.exe: D:\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.219\build.rs
-
-D:\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde-1.0.219\build.rs:
